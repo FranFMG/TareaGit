8d4a731a8a462bacc2c5008db02fdc86332c9ba5
Fran Madrid Garcia <frangarmad@gmail.com>
2023-05-25 20:01:38 +0200

    Verion 1.0 del proyecto

---
+++ src/main/java/com/mycompany/garciamadrid/GarciaMadrid.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.garciamadrid;
+
+/**
+ *
+ * @author Fran Madrid Garcia
+ */
+public class GarciaMadrid {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
